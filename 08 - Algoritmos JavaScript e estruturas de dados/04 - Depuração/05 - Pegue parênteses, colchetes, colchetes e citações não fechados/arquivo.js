7a3eeb7611b70d18c910620fa6472839dea71c89
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-01-12 17:07:58 -0300

    atualização

---
+++ 08 - Algoritmos JavaScript e estruturas de dados/04 - Depuração/05 - Pegue parênteses, colchetes, colchetes e citações não fechados/arquivo.js
@@ -0,0 +1,3 @@
+let myArray = [1, 2, 3]; // Adicionado colchete de fechamento ]
+let arraySum = myArray.reduce((previous, current) =>  previous + current); // Adicionado parêntese de fechamento )
+console.log(`Sum of array values is: ${arraySum}`);
